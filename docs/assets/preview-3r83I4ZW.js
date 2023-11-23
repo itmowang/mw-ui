8bce5debf90b4a80078f2ee9534e273f773385ce
Mowang <mw@loli.wang>
2023-11-23 17:40:20 +0800

    📃 docs: 修改文档

---
+++ docs/assets/preview-3r83I4ZW.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./iframe-NN-D798u.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-rLjFYIKh.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8]),import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-3r83I4ZW.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-rLjFYIKh.js","./iframe-NN-D798u.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-6f2S3HzO.js","./index-f2OjG03D.js","./index-mtoY0opN.js","./index-ogXoivrg.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
